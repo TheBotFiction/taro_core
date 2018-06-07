5c8df2e5dfad33a663aa8c753f5dbb73272f36ae
p.hoai.le <yeuem1vannam@users.noreply.github.com>
2018-06-08 00:35:37 +0900

    feat: leave migration files in engine

--- lib/taro_core/engine.rb
+++ lib/taro_core/engine.rb
@@ -1,5 +1,17 @@
 module TaroCore
   class Engine < ::Rails::Engine
+    # :nocov:
+    initializer :append_migrations do |app|
+      # This prevents migrations from being loaded twice from the inside of the
+      # gem itself (dummy test app)
+      if app.root.to_s !~ /#{root}/
+        config.paths["db/migrate"].expanded.each do |migration_path|
+          app.config.paths["db/migrate"] << migration_path
+        end
+      end
+    end
+    # :nocov:
+
     config.generators do |g|
       g.test_framework :rspec
       g.template_engine :haml
